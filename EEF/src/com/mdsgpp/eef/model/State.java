c2002cc65f8e579115cb58fb5aece71552d33c98
Miguel Nery <nery.mik@gmail.com>
2016-05-15 15:21:52 -0300

    Assertions made on State.java

--- EEF/src/com/mdsgpp/eef/model/State.java
+++ EEF/src/com/mdsgpp/eef/model/State.java
@@ -27,6 +27,10 @@ public class State {
 	}
 
 	public State(String stateName, String stateAbbreviation, HashMap<String, ArrayList<String[]>> information){
+		assert (stateName != null) : "null stateName";
+		assert (stateName != "" || stateName != " ") : "empty stateName";
+		assert (information != null) : "null information";
+
 		this.stateName = stateName;
 		this.stateAbbreviation = stateAbbreviation;
 
@@ -38,6 +42,7 @@ public class State {
 	}
 
 	public void setCensus(HashMap<String, ArrayList<String[]>> information) {
+		assert (information != null) : "null information";
 		ArrayList<String[]> elementaryFinalData;
 		ArrayList<String[]> elementaryInitialData;
 		ArrayList<String[]> highSchoolData;
@@ -50,6 +55,13 @@ public class State {
 		highSchoolData = information.get("censo_ensino_high_school");
 		ejaHighSchoolData = information.get("censo_eja_high_school");
 		ejaElementaryData = information.get("censo_eja_elementary");
+
+		assert (elementaryFinalData != null) : "null elementaryFinalData";
+		assert (elementaryInitialData != null) : "null elementaryInitialData";
+		assert (highSchoolData != null) : "null highSchoolData";
+		assert (ejaHighSchoolData != null) : "null ejaHighSchoolData";
+		assert (ejaElementaryData != null) : "null ejaElementaryData";
+
 
 		census = new Census[highSchoolData.size()];
 		for (int i=0; i<census.length; i++) {
@@ -76,9 +88,12 @@ public class State {
 	}
 
 	public void setPercentageCollaborationWithPIB(HashMap<String, ArrayList<String[]>> information) {
+		assert (information != null) : "null information";
 
 		double percentageCollaborationWithPIB[];
 		ArrayList<String[]> data = information.get("participacao_estadual_pib");
+
+		assert (data != null) : "null data";
 
 		percentageCollaborationWithPIB = new double[data.size()];
 		for (int i=0; i<data.size(); i++) {
@@ -93,8 +108,12 @@ public class State {
 	}
 
 	public void setStatePopulation(HashMap<String, ArrayList<String[]>> information) {
+		assert (information != null) : "null information";
 
 		ArrayList<String[]> data = information.get("populacao");
+
+		assert (data != null) : "null data";
+
 		this.statePopulation = Integer.parseInt(data.get(0)[1].replaceAll(",", "."));
 
 	}
@@ -109,6 +128,8 @@ public class State {
 	}
 
 	public void setIdebs(HashMap<String, ArrayList<String[]>> information) {
+		assert (information != null) : "null information";
+
 		ArrayList<String[]> elementaryFinalData;
 		ArrayList<String[]> elementaryInitialData;
 		ArrayList<String[]> highSchoolData;
@@ -117,6 +138,10 @@ public class State {
 		elementaryFinalData = information.get("5a_8a");
 		elementaryInitialData = information.get("series_iniciais");
 		highSchoolData = information.get("ensino_high_school");
+
+		assert (elementaryFinalData != null) : "null elementaryFinalData";
+		assert (elementaryInitialData != null) : "null elementaryInitialData";
+		assert (highSchoolData != null) : "null highSchoolData";
 
 		ideb = new Ideb[highSchoolData.size()];
 		for (int i=0; i<ideb.length; i++) {
@@ -144,12 +169,18 @@ public class State {
 	}
 
 	public Grade[] setGrade(HashMap<String, ArrayList<String[]>> information, String[] namesOfIndicative) {
+		assert (information != null) : "null information";
+		assert (namesOfIndicative != null) : "null namesOfIndicative";
+
 		ArrayList<String[]> elementarydata;
 		ArrayList<String[]> highSchoolData;
 		Grade readGrade[] = null;
 
 		elementarydata = information.get(namesOfIndicative[0]);
 		highSchoolData = information.get(namesOfIndicative[1]);
+
+		assert (elementarydata != null) : "null elementarydata";
+		assert (highSchoolData != null) : "null highSchoolData";
 
 		readGrade = new Grade[highSchoolData.size()];
 		for (int i=0; i<readGrade.length; i++) {
@@ -172,6 +203,9 @@ public class State {
 	}
 
 	public void setStudentGradesPerClass(HashMap<String, ArrayList<String[]>> information, String[] namesOfIndicative) {
+		assert (information != null) : "null information";
+		assert (namesOfIndicative != null) : "null namesOfIndicative";
+
 		this.studentGradesPerClass = this.setGrade(information, namesOfIndicative);
 	}
 
@@ -185,6 +219,9 @@ public class State {
 	}
 
 	public void setGradeClassHours(HashMap<String, ArrayList<String[]>> information, String[] namesOfIndicative) {
+		assert (information != null) : "null information";
+		assert (namesOfIndicative != null) : "null namesOfIndicative";
+
 		this.gradeClassHours = this.setGrade(information, namesOfIndicative);
 	}
 
@@ -198,6 +235,9 @@ public class State {
 	}
 
 	public void setAgeGradeDistortionRate(HashMap<String, ArrayList<String[]>> information, String[] namesOfIndicative) {
+		assert (information != null) : "null information";
+		assert (namesOfIndicative != null) : "null namesOfIndicative";
+
 		this.ageGradeDistortionRate = this.setGrade(information, namesOfIndicative);
 	}
 
@@ -237,13 +277,16 @@ public class State {
 	}
 
 	public Project[] setProjects(HashMap<String, ArrayList<String[]>> information, String[] namesOfIndicative) {
+		assert (information != null) : "null information";
+		assert (namesOfIndicative != null) : "null namesOfIndicative";
+
 		ArrayList<String[]> investedValuesData = null;
-		ArrayList<String[]> projectsAmountData = null;
+		ArrayList<String[]> projectsQuantityData = null;
 		Project readProjects[] = null;
 
 		if(information.containsKey(namesOfIndicative[0])) {
-			projectsAmountData = information.get(namesOfIndicative[0]);
-			readProjects = new Project[projectsAmountData.size()];
+			projectsQuantityData = information.get(namesOfIndicative[0]);
+			readProjects = new Project[projectsQuantityData.size()];
 			investedValuesData = information.get(namesOfIndicative[1]);
 		} else {
 			//do nothing
@@ -253,11 +296,11 @@ public class State {
 			readProjects[i] = new Project();
 			readProjects[i].setState(this);
 			if(i<readProjects.length-1) {
-				readProjects[i].setProjectYear(Integer.parseInt(projectsAmountData.get(i)[0]));
+				readProjects[i].setProjectYear(Integer.parseInt(projectsQuantityData.get(i)[0]));
 			} else {
 				//do nothing
 			}
-			readProjects[i].setProjectQuantity(Integer.parseInt(projectsAmountData.get(i)[1].replaceAll(",", ".")));
+			readProjects[i].setProjectQuantity(Integer.parseInt(projectsQuantityData.get(i)[1].replaceAll(",", ".")));
 			readProjects[i].setProjectValue(Double.parseDouble(investedValuesData.get(i)[1].replaceAll(",", ".")));
 		}
 
@@ -274,6 +317,9 @@ public class State {
 	}
 
 	public void setScienceAndThecnologyProjects(HashMap<String, ArrayList<String[]>> information, String[] namesOfIndicative) {
+		assert (information != null) : "null information";
+		assert (namesOfIndicative != null) : "null namesOfIndicative";
+
 		this.scienceAndThecnologyProjects = this.setProjects(information, namesOfIndicative);
 	}
 
@@ -287,6 +333,9 @@ public class State {
 	}
 
 	public void setPrimeirosProjetos(HashMap<String, ArrayList<String[]>> information, String[] namesOfIndicative) {
+		assert (information != null) : "null information";
+		assert (namesOfIndicative != null) : "null namesOfIndicative";
+
 		this.primeirosProjetos = this.setProjects(information, namesOfIndicative);
 	}
 
@@ -299,6 +348,9 @@ public class State {
 	}
 
 	public void setProjectsInct(HashMap<String, ArrayList<String[]>> information, String[] namesOfIndicative) {
+		assert (information != null) : "null information";
+		assert (namesOfIndicative != null) : "null namesOfIndicative";
+
 		this.inctProject = this.setProjects(information, namesOfIndicative);
 	}
 
@@ -311,6 +363,9 @@ public class State {
 	}
 
 	public void setApoioCnpqProject(HashMap<String, ArrayList<String[]>> information, String[] namesOfIndicative) {
+		assert (information != null) : "null information";
+		assert (namesOfIndicative != null) : "null namesOfIndicative";
+
 		this.apoioCnpqProject = this.setProjects(information, namesOfIndicative);
 	}
 
@@ -323,6 +378,9 @@ public class State {
 	}
 
 	public void setJovensPesquisadoresProject(HashMap<String, ArrayList<String[]>> information, String[] namesOfIndicative) {
+		assert (information != null) : "null information";
+		assert (namesOfIndicative != null) : "null namesOfIndicative";
+
 		this.jovensPesquisadoresProject = this.setProjects(information, namesOfIndicative);
 	}
 
@@ -335,6 +393,9 @@ public class State {
 	}
 
 	public void setEducationalAchievementRate(HashMap<String, ArrayList<String[]>> information, String[] namesOfIndicative) {
+		assert (information != null) : "null information";
+		assert (namesOfIndicative != null) : "null namesOfIndicative";
+
 		this.educationalAchievementRate = this.setGrade(information, namesOfIndicative);
 	}
 
@@ -347,10 +408,15 @@ public class State {
 	}
 
 	public void setSchoolDropoutRate(HashMap<String, ArrayList<String[]>> information, String[] namesOfIndicative) {
+		assert (information != null) : "null information";
+		assert (namesOfIndicative != null) : "null namesOfIndicative";
+
 		this.schoolDropoutRate = this.setGrade(information, namesOfIndicative);
 	}
 
 	public void fillData(HashMap<String, ArrayList<String[]>> information) {
+		assert (information != null) : "null information";
+
 		String[] gradeClassHoursIndicatives = { "horas_aula_ensino_elementary", "horas_aula_ensino_high_school" };
 		String[] studentGradesPerClassIndicatives = { "alunos_por_turma_ensino_elementary", "alunos_por_turma_ensino_high_school" };
 		String[] scienceAndThecnologyProjectsIndicatives = { "numero_projetos", "valor_investido" };
@@ -361,6 +427,18 @@ public class State {
 		String[] ageGradeDistortionRate = { "distortion_rate_elementary", "distortion_rate_ensino_high_school" };
 		String[] educationalAchievementRate = { "taxa_aprovacao_elementary", "taxa_aprovacao_high_school" };
 		String[] schoolDropoutRate = { "taxa_abandono_elementary", "taxa_abandono_high_school" };
+
+		assert (gradeClassHoursIndicatives != null) : "null gradeClassHoursIndicatives";
+		assert (studentGradesPerClassIndicatives != null) : "null studentGradesPerClassIndicatives";
+		assert (scienceAndThecnologyProjectsIndicatives != null) : "null scienceAndThecnologyProjectsIndicatives";
+		assert (primeirosProjetosIndicatives != null) : "null primeirosProjetosIndicatives";
+		assert (apoioCnpqProjectIndicatives != null) : "null apoioCnpqProjectIndicatives";
+		assert (jovensPesquisadoresProjectIndicatives != null) : "null jovensPesquisadoresProjectIndicatives";
+		assert (inctProjectsIndicatives != null) : "null inctProjectsIndicatives";
+		assert (ageGradeDistortionRate != null) : "null ageGradeDistortionRate";
+		assert (educationalAchievementRate != null) : "null educationalAchievementRate";
+		assert (schoolDropoutRate != null) : "null schoolDropoutRate";
+
 
 		this.setStatePopulation(information);
 		this.setCensus(information);
