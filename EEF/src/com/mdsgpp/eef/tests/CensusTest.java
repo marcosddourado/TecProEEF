e9de985ca54d5d679311ca64160b0828183d84dc
Miguel Nery <nery.mik@gmail.com>
2016-06-06 21:24:53 -0300

    sample working test added

--- EEF/src/com/mdsgpp/eef/tests/CensusTest.java
+++ EEF/src/com/mdsgpp/eef/tests/CensusTest.java
@@ -1,9 +1,19 @@
 package com.mdsgpp.eef.tests;
 
+import org.junit.Test;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.CoreMatchers.*;
+
 /**
  * Created by andreanmasiro on 06/06/16.
  */
 public class CensusTest {
+
+    @Test
+    public void trueIsTrue() {
+        assert(true == true);
+        assertThat(true, is(true));
+    }
 
         
 }
