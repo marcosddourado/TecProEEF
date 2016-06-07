114e88fa4417d5074bd3127a3397702630092635
Miguel Nery <nery.mik@gmail.com>
2016-06-06 21:59:41 -0300

    mockito library added

--- EEF/src/com/mdsgpp/eef/tests/CensusTest.java
+++ EEF/src/com/mdsgpp/eef/tests/CensusTest.java
@@ -3,17 +3,23 @@ package com.mdsgpp.eef.tests;
 import org.junit.Test;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.CoreMatchers.*;
+import org.mockito.Mock;
+
 
 /**
  * Created by andreanmasiro on 06/06/16.
  */
 public class CensusTest {
 
+    @Mock
+    String hue = "haha";
+
     @Test
     public void trueIsTrue() {
         assert(true == true);
-        assertThat(true, is(true));
+        assertThat(10, is(10));
     }
+
 
         
 }
