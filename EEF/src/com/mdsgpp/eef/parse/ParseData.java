27999f8c7c31c96fb9719d4456587b12e2a0de7c
Miguel Nery <nery.mik@gmail.com>
2016-06-08 00:11:38 -0300

    refatoration on class structure

--- EEF/src/com/mdsgpp/eef/parse/ParseData.java
+++ EEF/src/com/mdsgpp/eef/parse/ParseData.java
@@ -12,7 +12,6 @@ import android.content.res.AssetManager;
 
 public class ParseData {
 
-	private HashMap<String, ArrayList<String[]>> informations; // Container for every educational indicative available for the state.
 	private ArrayList<String[]> data;
 	private Context context;
 	private String indicatorName;
@@ -33,13 +32,13 @@ public class ParseData {
 	public ParseData(Context context) {
 		assert(context != null) : "null context passed on constructor";
 		this.context = context;
-		this.informations = new HashMap<String, ArrayList<String[]>>();
 	}
 
 	public HashMap<String, ArrayList<String[]>> getState(int position) throws IOException {
 		assert(position >= 0) : "position must be positive. position was" + position;
 		assert(position <= STATES_MAX_INDEX) : "position can't be greater than 26. position was" + position;
 		String name, acronym;
+		HashMap<String, ArrayList<String[]>> state_data = new HashMap<String, ArrayList<String[]>>();
 
 		AssetManager am = this.context.getAssets();
 		InputStream is = am.open(this.states[position] + this.EXTENSION);
@@ -48,19 +47,12 @@ public class ParseData {
 		assert (br != null) : "buffer cannot reference NULL";
 
 		name = br.readLine();
-		acronym = br.readLine();																																																																																																																																																																																																																																																																																														
+		acronym = br.readLine();
 
-		eraseInformations();
-		eraseData();
+		insertAcronymName(name, acronym, state_data);
+		readIndicatives(br, state_data);
 
-		insertAcronymName(name, acronym);
-		readIndicatives(br);
-
-		return informations;
-	}
-
-	public void eraseInformations() {
-		this.informations.clear();
+		return state_data;
 	}
 
 	public void eraseData() {
@@ -68,7 +60,7 @@ public class ParseData {
 	}
 
 	// Method responsible for sending state name and acronym through indicatives hashmap.
-	public void insertAcronymName(String name, String acronym) {
+	public void insertAcronymName(String name, String acronym, HashMap<String, ArrayList<String[]>> state_data) {
 		ArrayList<String[]> container = new ArrayList<String[]>();
 		String nameAndAcronym[] = new String[PAIR];
 		nameAndAcronym[NAME_POSITION] = name;
@@ -78,11 +70,11 @@ public class ParseData {
 
 		assert (container != null): "Dictionary input cannot be of NULL value!";
 
-		this.informations.put("nome_e_sigla", container);
+		state_data.put("nome_e_sigla", container);
 	}
 
 	// Method responsble for reading available data.
-	public void  readIndicatives(BufferedReader br) throws IOException {
+	public void  readIndicatives(BufferedReader br, HashMap<String, ArrayList<String[]>> state_data) throws IOException {
 		int aux = 0;
 		String line;
 
@@ -100,7 +92,7 @@ public class ParseData {
 
 			if (aux == LINES) {
 				aux = 0;
-				this.informations.put(indicatorName, data);
+				state_data.put(indicatorName, data);
 				indicatorName = br.readLine();
 				eraseData();
 			}
