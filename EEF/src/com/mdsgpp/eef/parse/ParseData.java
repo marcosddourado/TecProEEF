a78e0a4faca59634f8d444cb48402baa878ba15f
Miguel Nery <nery.mik@gmail.com>
2016-06-10 20:22:07 -0300

    returned to stable version of class

--- EEF/src/com/mdsgpp/eef/parse/ParseData.java
+++ EEF/src/com/mdsgpp/eef/parse/ParseData.java
@@ -6,13 +6,13 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.HashMap;
-import com.mdsgpp.eef.model.State;
 
 import android.content.Context;
 import android.content.res.AssetManager;
 
 public class ParseData {
 
+	private HashMap<String, ArrayList<String[]>> informations; // Container for every educational indicative available for the state.
 	private ArrayList<String[]> data;
 	private Context context;
 	private String indicatorName;
@@ -24,19 +24,25 @@ public class ParseData {
 	private final int ACRONYM_POSITION = 1;
 	private final int STATES_MAX_INDEX = 26; // index counting starts with 0, so the maximum index for the 27 states is 26
 
+	private final String states[] = { "Acre", "Alagoas", "Amapa", "Amazonas", "Bahia", "Ceara",
+			"Distrito Federal", "Espirito Santo", "Goias", "Maranhao", "Mato Grosso do Sul",
+			"Mato Grosso", "Minas Gerais", "Para", "Paraiba", "Parana", "Pernambuco", "Piaui",
+			"Rio de Janeiro", "Rio Grande do Norte", "Rio Grande do Sul", "Rondonia", "Roraima",
+			"Santa Catarina", "Sao Paulo", "Sergipe", "Tocantins" };
+
 	public ParseData(Context context) {
 		assert(context != null) : "null context passed on constructor";
 		this.context = context;
+		this.informations = new HashMap<String, ArrayList<String[]>>();
 	}
 
 	public HashMap<String, ArrayList<String[]>> getState(int position) throws IOException {
 		assert(position >= 0) : "position must be positive. position was" + position;
 		assert(position <= STATES_MAX_INDEX) : "position can't be greater than 26. position was" + position;
 		String name, acronym;
-		HashMap<String, ArrayList<String[]>> state_data = new HashMap<String, ArrayList<String[]>>();
 
 		AssetManager am = this.context.getAssets();
-		InputStream is = am.open(State.states[position] + this.EXTENSION);
+		InputStream is = am.open(this.states[position] + this.EXTENSION);
 		BufferedReader br = new BufferedReader(new InputStreamReader(is));
 
 		assert (br != null) : "buffer cannot reference NULL";
@@ -44,10 +50,17 @@ public class ParseData {
 		name = br.readLine();
 		acronym = br.readLine();
 
-		insertAcronymName(name, acronym, state_data);
-		readIndicatives(br, state_data);
+		eraseInformations();
+		eraseData();
 
-		return state_data;
+		insertAcronymName(name, acronym);
+		readIndicatives(br);
+
+		return informations;
+	}
+
+	public void eraseInformations() {
+		this.informations.clear();
 	}
 
 	public void eraseData() {
@@ -55,7 +68,7 @@ public class ParseData {
 	}
 
 	// Method responsible for sending state name and acronym through indicatives hashmap.
-	public void insertAcronymName(String name, String acronym, HashMap<String, ArrayList<String[]>> state_data) {
+	public void insertAcronymName(String name, String acronym) {
 		ArrayList<String[]> container = new ArrayList<String[]>();
 		String nameAndAcronym[] = new String[PAIR];
 		nameAndAcronym[NAME_POSITION] = name;
@@ -65,11 +78,11 @@ public class ParseData {
 
 		assert (container != null): "Dictionary input cannot be of NULL value!";
 
-		state_data.put("nome_e_sigla", container);
+		this.informations.put("nome_e_sigla", container);
 	}
 
 	// Method responsble for reading available data.
-	public void  readIndicatives(BufferedReader br, HashMap<String, ArrayList<String[]>> state_data) throws IOException {
+	public void  readIndicatives(BufferedReader br) throws IOException {
 		int aux = 0;
 		String line;
 
@@ -87,7 +100,7 @@ public class ParseData {
 
 			if (aux == LINES) {
 				aux = 0;
-				state_data.put(indicatorName, data);
+				this.informations.put(indicatorName, data);
 				indicatorName = br.readLine();
 				eraseData();
 			}
