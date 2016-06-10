a063d274b6d19b4788d00f85cb45fafda2dda96e
Miguel Nery <nery.mik@gmail.com>
2016-06-09 21:17:11 -0300

    method refactored

--- EEF/src/com/mdsgpp/eef/parse/ParseData.java
+++ EEF/src/com/mdsgpp/eef/parse/ParseData.java
@@ -32,17 +32,8 @@ public class ParseData {
 	public HashMap<String, ArrayList<String[]>> getState(int position) throws IOException {
 		assert(position >= 0) : "position must be positive. position was" + position;
 		assert(position <= STATES_MAX_INDEX) : "position can't be greater than 26. position was" + position;
-		String name, acronym;
+
 		HashMap<String, ArrayList<String[]>> state_data = new HashMap<String, ArrayList<String[]>>();
-
-		AssetManager am = this.context.getAssets();
-		InputStream is = am.open(State.states[position] + this.EXTENSION);
-		BufferedReader br = new BufferedReader(new InputStreamReader(is));
-
-		assert (br != null) : "buffer cannot reference NULL";
-
-		name = br.readLine();
-		acronym = br.readLine();
 
 		insertAcronymName(name, acronym, state_data);
 		readIndicatives(br, state_data);
@@ -50,22 +41,34 @@ public class ParseData {
 		return state_data;
 	}
 
-	public void eraseData() {
-		this.data = new ArrayList<String[]>();
+	public BufferedReader readState(int state_position) throws IOException {
+		AssetManager am = this.context.getAssets();
+		InputStream is = am.open(State.states[state_position] + this.EXTENSION);
+		BufferedReader br = new BufferedReader(new InputStreamReader(is));
+
+		return br;
 	}
 
 	// Method responsible for sending state name and acronym through indicatives hashmap.
-	public void insertAcronymName(String name, String acronym, HashMap<String, ArrayList<String[]>> state_data) {
-		ArrayList<String[]> container = new ArrayList<String[]>();
+	public void insertAcronymName(int state_position, HashMap<String, ArrayList<String[]>> state_data) throws IOException {
 		String nameAndAcronym[] = new String[PAIR];
+		ArrayList<String[]> container = new ArrayList<String[]>();
+		String name, acronym;
+
+		BufferedReader br = readState(state_position);
+
+		name = br.readLine();
+		acronym = br.readLine();
+
 		nameAndAcronym[NAME_POSITION] = name;
 		nameAndAcronym[ACRONYM_POSITION] = acronym;
-
 		container.add(nameAndAcronym);
 
-		assert (container != null): "Dictionary input cannot be of NULL value!";
-
 		state_data.put("nome_e_sigla", container);
+	}
+
+	public void eraseData() {
+		this.data = new ArrayList<String[]>();
 	}
 
 	// Method responsble for reading available data.
