b7ef77598d15a1e77cd96a19d3e28e0e92dc4068
Miguel Nery <nery.mik@gmail.com>
2016-06-11 14:49:04 -0300

    refactoration

--- EEF/src/com/mdsgpp/eef/parse/ParseData.java
+++ EEF/src/com/mdsgpp/eef/parse/ParseData.java
@@ -10,6 +10,8 @@ import java.util.HashMap;
 import android.content.Context;
 import android.content.res.AssetManager;
 
+import com.mdsgpp.eef.model.State;
+
 public class ParseData {
 
 	private HashMap<String, ArrayList<String[]>> informations; // Container for every educational indicative available for the state.
@@ -24,11 +26,6 @@ public class ParseData {
 	private final int ACRONYM_POSITION = 1;
 	private final int STATES_MAX_INDEX = 26; // index counting starts with 0, so the maximum index for the 27 states is 26
 
-	private final String states[] = { "Acre", "Alagoas", "Amapa", "Amazonas", "Bahia", "Ceara",
-			"Distrito Federal", "Espirito Santo", "Goias", "Maranhao", "Mato Grosso do Sul",
-			"Mato Grosso", "Minas Gerais", "Para", "Paraiba", "Parana", "Pernambuco", "Piaui",
-			"Rio de Janeiro", "Rio Grande do Norte", "Rio Grande do Sul", "Rondonia", "Roraima",
-			"Santa Catarina", "Sao Paulo", "Sergipe", "Tocantins" };
 
 	public ParseData(Context context) {
 		assert(context != null) : "null context passed on constructor";
@@ -42,7 +39,7 @@ public class ParseData {
 		String name, acronym;
 
 		AssetManager am = this.context.getAssets();
-		InputStream is = am.open(this.states[position] + this.EXTENSION);
+		InputStream is = am.open(State.states[position] + this.EXTENSION);
 		BufferedReader br = new BufferedReader(new InputStreamReader(is));
 
 		assert (br != null) : "buffer cannot reference NULL";
