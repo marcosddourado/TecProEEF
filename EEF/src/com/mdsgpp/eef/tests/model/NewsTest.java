f8e1b3f5300894fd8c2042cc24d723935ace0e29
andreanmasiro <andreanmasiro@live.com>
2016-06-23 20:48:23 -0300

    NewsTest.java done

--- EEF/src/com/mdsgpp/eef/tests/model/NewsTest.java
+++ EEF/src/com/mdsgpp/eef/tests/model/NewsTest.java
@@ -11,7 +11,6 @@ import org.mockito.Mockito;
 import java.util.Random;
 
 import static org.hamcrest.CoreMatchers.is;
-import static org.hamcrest.CoreMatchers.notNullValue;
 import static org.hamcrest.CoreMatchers.nullValue;
 import static org.hamcrest.MatcherAssert.assertThat;
 
@@ -41,5 +40,62 @@ public class NewsTest {
         assertThat(news.getNewsDescription(), is(nullValue()));
         assertThat(news.getNewsLink(), is(nullValue()));
         assertThat(news.getNewsDate(), is(nullValue()));
+    }
+
+    @Test
+    public void testSetNewsProperties() {
+
+        String title = "Title";
+        String category = "Category";
+        String description = "Description";
+        String link = "Link";
+        String date = "Date";
+
+        news.setNewsTitle(title);
+        news.setNewsCategory(category);
+        news.setNewsDescription(description);
+        news.setNewsLink(link);
+        news.setNewsDate(date);
+
+        assertThat(news.getNewsTitle(), is(title));
+        assertThat(news.getNewsCategory(), is(category));
+        assertThat(news.getNewsDescription(), is(description));
+        assertThat(news.getNewsLink(), is(link));
+        assertThat(news.getNewsDate(), is(date));
+    }
+
+    @Test (expected = AssertionError.class)
+    public void testSetInvalidTitle() {
+
+        news.setNewsTitle("");
+        news.setNewsTitle(null);
+    }
+
+    @Test (expected = AssertionError.class)
+    public void testSetInvalidCategory() {
+
+        news.setNewsCategory("");
+        news.setNewsCategory(null);
+    }
+
+    @Test (expected = AssertionError.class)
+    public void testSetInvalidDescription() {
+
+        news.setNewsDescription("");
+        news.setNewsDescription(null);
+    }
+
+    @Test (expected = AssertionError.class)
+    public void testSetInvalidLink() {
+
+        news.setNewsLink("");
+        news.setNewsLink(null);
+    }
+
+    @Test (expected = AssertionError.class)
+    public void testSetInvalidDate() {
+
+        news.setNewsDate("");
+        news.setNewsDate(null);
     }
 }
