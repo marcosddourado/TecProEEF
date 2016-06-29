af3e4efe1a6f43cd454140d27eeaa253c0e72149
Miguel Nery <nery.mik@gmail.com>
2016-06-28 22:20:31 -0300

    API comments made

--- EEF/src/com/mdsgpp/eef/parse/PersistenceFeed.java
+++ EEF/src/com/mdsgpp/eef/parse/PersistenceFeed.java
@@ -15,37 +15,59 @@ public class PersistenceFeed {
 	private static final String FILENAME = "feed.txt";
 	private static PersistenceFeed instance = null;
 	private Context context = null;
-	
+
+	/**
+	 * Class's constructor
+	 * @param context
+     */
 	public PersistenceFeed(Context context) {
 		assert(context != null) : "null context";
 		this.context = context;
 	}
 
+	/**
+	 * Return constructed instance of PersistenceFeed
+	 * @param context
+	 * @return
+     */
 	public static PersistenceFeed getInstance(Context context) {
 		if (instance == null) {
-			assert(context != null) : "null context";
+			//Construct class if it wasn't already
 			instance = new PersistenceFeed(context);
 		} else {/*do nothing*/}
 		return instance;
 	}
-	//Rewrites a data file.
+
+	/**
+	 *Write feed file. Overwrite it if it already exists
+	 * @param feed
+	 * @throws IOException
+	 * @throws NullPointerException
+     */
 	public void writeFeedFile(Feed feed) throws IOException, NullPointerException {
 		assert(context != null) : "null context";
+		//Opening file
 		FileOutputStream fileOut = this.context.openFileOutput(FILENAME, Context.MODE_PRIVATE);
 		ObjectOutputStream out = new ObjectOutputStream(fileOut);
-
-		assert(out != null) : "Output cannot be null!";
 
 		out.writeObject(feed);
 		out.close();
 	}
 
-	//Reads JSON data file and returns a serialized version of the data.
+	/**
+	 * Return Feed object read from file
+	 * @return
+	 * @throws IOException
+	 * @throws ClassNotFoundException
+     */
 	public Feed readFeedFile() throws IOException, ClassNotFoundException {
 		Feed feed;
+
+		//Opening file
 		FileInputStream fileIn = this.context.openFileInput(FILENAME);
 		ObjectInputStream in = new ObjectInputStream(fileIn);
 
+		//Filling object with read data
 		feed = (Feed) in.readObject();
 		in.close();
 		
