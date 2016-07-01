f8654004eac801d075d5c802823dd546cfc5bf1c
Marcos Dourado <marcosdourado92@gmail.com>
2016-06-30 23:18:54 -0300

    testing State's method emptyDouble()

--- EEF/src/com/mdsgpp/eef/tests/model/StateTest.java
+++ EEF/src/com/mdsgpp/eef/tests/model/StateTest.java
@@ -1,8 +1,28 @@
 package com.mdsgpp.eef.tests.model;
 
-/**
- * Created by miknery on 30/06/16.
- */
-public class StateTest {
+import com.mdsgpp.eef.model.State;
 
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.Random;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+public class StateTest {
+    private State state;
+    Random random = new Random();
+
+    @Before
+    public void setup() {
+        this.state = new State();
+    }
+
+    @Test
+    public void testEmptyDouble() {
+        double empty[] = {0};
+
+        assertThat(state.emptyDouble(), is(empty));
+    }
 }
