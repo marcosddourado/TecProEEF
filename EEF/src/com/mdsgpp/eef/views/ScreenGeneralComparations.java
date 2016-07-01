439818c9cfaa4fa68780820453ca0df83a1a3933
André Bedran <bedran.fleck@gmail.com>
2016-07-01 01:17:24 -0300

    Adding javadoc for general comparison screen.

--- EEF/src/com/mdsgpp/eef/views/ScreenGeneralComparations.java
+++ EEF/src/com/mdsgpp/eef/views/ScreenGeneralComparations.java
@@ -12,19 +12,33 @@ import android.view.View;
 
 public class ScreenGeneralComparations extends Activity {
 
+	/**
+	 * Sets the content view
+	 * @param savedInstanceState Current app state.
+	 */
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_tela_comparacoes_gerais);
 	}
 
+	/**
+	 * Inflates a menu
+	 * @param menu Menu reference.
+	 * @return Should inflate menu.
+	 */
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.menu_screens, menu);
 		return true;
 	}
-	
+
+	/**
+	 * Default item clicked behavior. Navigates up or loads about screen.
+	 * @param item Clicked item.
+	 * @return Should navigate.
+	 */
     public boolean onOptionsItemSelected(MenuItem item) {
     	
 		switch (item.getItemId()) {
@@ -41,17 +55,28 @@ public class ScreenGeneralComparations extends Activity {
 		
     	return true;
     }
-    
+
+    /**
+	 * Loads about screen.
+	 */
     public void loadAboutScreen() {
     	Intent intent = new Intent(this, ScreenAboutGeneralComparisons.class);
     	startActivity(intent);
     }
-	
+
+	/**
+	 * Starts comparison activity.
+	 * @param view Wildcard view reference.
+     */
 	public void clickButtonComparation(View view) {
 		Intent intent = new Intent(this, ScreenComparation.class);
 		startActivity(intent);
 	}
-	
+
+	/**
+	 * Loads screen to choose indicatives again.
+	 * @param view Wildcard view reference.
+     */
 	public void clickButtonIndicatives(View view) {
 		Intent intent = new Intent(this, ScreenQueryPerIndicative.class);
 		startActivity(intent);
