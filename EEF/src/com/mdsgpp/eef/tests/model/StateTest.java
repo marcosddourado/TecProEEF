fbf77a47849db909ddac0f33edd2514403a496f6
Marcos Dourado <marcosdourado92@gmail.com>
2016-06-30 23:43:37 -0300

    fixing bug on emptyIdeb test

--- EEF/src/com/mdsgpp/eef/tests/model/StateTest.java
+++ EEF/src/com/mdsgpp/eef/tests/model/StateTest.java
@@ -1,5 +1,6 @@
 package com.mdsgpp.eef.tests.model;
 
+import com.mdsgpp.eef.model.Census;
 import com.mdsgpp.eef.model.Ideb;
 import com.mdsgpp.eef.model.State;
 
@@ -31,11 +32,36 @@ public class StateTest {
     }
 
     @Test
-    public void testEmptyIdeb(){
-        Ideb empty[] = {new Ideb(0.0, 0.0, 0.0)};
+    public void testEmptyIdeb() {
+        final int UNIC_POSITION = 0;
+        Ideb emptyIdebTest[] = {new Ideb(0.0, 0.0, 0.0)};
+        Ideb stateIdebTest[] = state.emptyIdeb();
 
-        assertThat(state.emptyIdeb(), is(empty));
+        //Testing elementary values
+        double elementaryTest = emptyIdebTest[UNIC_POSITION].getElementary();
+        double elementaryState = stateIdebTest[UNIC_POSITION].getElementary();
+
+        assertThat(elementaryTest, is(elementaryState));
+
+        //Testing high school values
+        double highSchoolTest = emptyIdebTest[UNIC_POSITION].getHighSchool();
+        double highSchoolState = stateIdebTest[UNIC_POSITION].getHighSchool();
+
+        assertThat(highSchoolTest, is(highSchoolState));
+
+        //Testing high school values
+        double initialGradesTest = emptyIdebTest[UNIC_POSITION].getInitialGrades();
+        double initialGradesState = stateIdebTest[UNIC_POSITION].getInitialGrades();
+
+        assertThat(initialGradesTest, is(initialGradesState));
 
     }
+
+//    @Test
+//    public void testEmptyCensus() {
+//        Census empty[] = {new Census(0.0, 0.0, 0.0, 0.0, 0.0)};
+//
+//        assertThat(state.emptyCensus(), is(empty));
+//    }
 
 }
