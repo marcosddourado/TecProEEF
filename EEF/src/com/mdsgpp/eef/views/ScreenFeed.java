7e8b79b8cf49ad692a9b6d2b5a90bf1e62c3d254
André Bedran <bedran.fleck@gmail.com>
2016-07-01 01:10:24 -0300

    Adding javadoc to ScreenFeed class.

--- EEF/src/com/mdsgpp/eef/views/ScreenFeed.java
+++ EEF/src/com/mdsgpp/eef/views/ScreenFeed.java
@@ -19,7 +19,11 @@ import android.widget.ListView;
 public class ScreenFeed extends Activity implements DataReceiver {
 
 	private final static String FEED_ADDRESS = "http://noticias.gov.br/noticias/rss?id=AFSZW";
-	
+
+	/**
+	 * Sets the content view
+	 * @param savedInstanceState Current app state.
+	 */
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -29,13 +33,23 @@ public class ScreenFeed extends Activity implements DataReceiver {
 		new FeedController(this, this).execute(FEED_ADDRESS);
 	}
 
+	/**
+	 * Inflates a menu
+	 * @param menu Menu reference.
+	 * @return Should inflate menu.
+	 */
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.menu_screens, menu);
 		return true;
 	}
-	
+
+	/**
+	 * Default item clicked behavior. Navigates Up.
+	 * @param item Clicked item.
+	 * @return Should navigate.
+	 */
 	@Override
     public boolean onOptionsItemSelected(MenuItem item) {
 		switch (item.getItemId()) {
@@ -52,6 +66,10 @@ public class ScreenFeed extends Activity implements DataReceiver {
     	return true;
     }
 
+	/**
+	 * Receives a Feed object and retrieves data from it.
+	 * @param feed The input Feed.
+     */
 	@Override
 	public void receiveFeed(final Feed feed) {
 		ListView listview = (ListView) findViewById(R.id.listView1);
@@ -71,7 +89,10 @@ public class ScreenFeed extends Activity implements DataReceiver {
 			}
 		});
 	}
-	
+
+	/**
+	 * Loads the about screen.
+	 */
 	public void loadAboutScreen() {
 		Intent intent = new Intent(this, ScreenFeedAbout.class);
 		startActivity(intent);
