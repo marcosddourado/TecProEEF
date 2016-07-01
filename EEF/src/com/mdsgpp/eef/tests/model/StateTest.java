5ecd129bc766fbb94e35e29a8d588cb25523d1f2
Marcos Dourado <marcosdourado92@gmail.com>
2016-07-01 00:00:28 -0300

    fixing bug on testEmptyCensus

--- EEF/src/com/mdsgpp/eef/tests/model/StateTest.java
+++ EEF/src/com/mdsgpp/eef/tests/model/StateTest.java
@@ -37,6 +37,8 @@ public class StateTest {
         Ideb emptyIdebTest[] = {new Ideb(0.0, 0.0, 0.0)};
         Ideb stateIdebTest[] = state.emptyIdeb();
 
+        //Testing the values for all the Ideb's attributes
+
         //Testing elementary values
         double elementaryTest = emptyIdebTest[UNIC_POSITION].getElementary();
         double elementaryState = stateIdebTest[UNIC_POSITION].getElementary();
@@ -57,11 +59,37 @@ public class StateTest {
 
     }
 
-//    @Test
-//    public void testEmptyCensus() {
-//        Census empty[] = {new Census(0.0, 0.0, 0.0, 0.0, 0.0)};
-//
-//        assertThat(state.emptyCensus(), is(empty));
-//    }
+    @Test
+    public void testEmptyCensus() {
+        final int UNIC_POSITION = 0;
+        Census emptyCensusTest[] = {new Census(0.0, 0.0, 0.0, 0.0, 0.0)};
+        Census emptyCensusState[] = state.emptyCensus();
+
+        //Testing the values for all the Census's attributes
+
+        //Testing the values for the initial elementary years
+        double initialElementaryYearsTest = emptyCensusTest[UNIC_POSITION].getInitialElementaryYears();
+        double initialElementaryYearsState = emptyCensusState[UNIC_POSITION].getInitialElementaryYears();
+
+        assertThat(initialElementaryYearsTest, is(initialElementaryYearsState));
+
+        //Testing the values for the final elementary years
+        double finalElementaryYearsTest = emptyCensusTest[UNIC_POSITION].getFinalElementaryYears();
+        double finalElementaryYearsState = emptyCensusState[UNIC_POSITION].getFinalElementaryYears();
+
+        assertThat(finalElementaryYearsTest, is(finalElementaryYearsState));
+
+        //Testing the values for high school
+        double highSchoolTest = emptyCensusTest[UNIC_POSITION].getHighSchool();
+        double highSchoolState = emptyCensusState[UNIC_POSITION].getHighSchool();
+
+        assertThat(highSchoolTest, is(highSchoolState));
+
+        //Testing the values for EJA's high school
+        double highSchoolEJATest = emptyCensusTest[UNIC_POSITION].getHighSchoolEJA();
+        double highSchoolEJAState = emptyCensusState[UNIC_POSITION].getHighSchoolEJA();
+
+        assertThat(highSchoolEJATest, is(highSchoolEJAState));
+    }
 
 }
